57b759a961aa43c2ff4419e66a04fda7a60d394d
TEUN\teuna <teunaarts@live.nl>
2017-09-27 11:30:25 +0200

    website goed geordend

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+@include('header.html');
+?>
+
+<div class="image">
+    <h1>Dit is de Duo opdracht</h1>
+</div>
+
+<div class="container">
+
+    <h1>Home</h1>
+
+</div>
+
+
+<?php
+@include('footer.html');
+?>
